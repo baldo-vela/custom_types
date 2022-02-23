8ed6e214897eb505ade86afc58c5e7661e89e1f5
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-02-23 15:34:23 -0600

    Adds more pedantic documentation

--- generic_functions/index.ts
+++ generic_functions/index.ts
@@ -8,6 +8,15 @@ function getFilledArray<T>(value: T, n: number): T[] {
   let coordinateArray: [number, number][];
   
   // Write your code below:
-  
+/*   Use the function getFilledArray<T>() to assign values to the variables stringArray, numberArray, personArray, and coordinateArray.
+
+Write your code so that:
+
+Each value should be an array of 6 elements.
+All elements in stringArray should equal 'hi'.
+All elements in numberArray should equal 9.
+All elements in personArray should equal {name: 'J. Dean', age: 24}.
+All elements in coordinateArray should equal [3,4].
+Don’t forget to specify the value of T! */
   
   
